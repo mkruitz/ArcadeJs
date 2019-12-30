580daebf165631c354e3c5fb049cc5e7001fc445
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2019-12-30 19:53:52 +0100

    Assets Upgrade ball, add pad and bonus sprites

--- src/assets.js
+++ src/assets.js
@@ -10,7 +10,15 @@ var game_assets = {
           tile_v_single: [3, 6, 1, 2],
           tile_mirror: [3, 0, 2, 2],
           tile_box: [3, 3, 2, 3],
-          ball_standard: [6, 1, 1, 1]
+         
+          bonus_pid: [6, 0, 1, 1],
+          ball_org: [6, 1, 1, 1],
+          ball_standard: [7, 1, 1, 1],
+         
+          pad_s: [5, 3, 3, 1],
+          pad_n: [5, 4, 4, 1],
+          pad_l: [5, 5, 5, 1],
+          pad_xl: [5, 6, 6, 1]
         }
       }
     }
