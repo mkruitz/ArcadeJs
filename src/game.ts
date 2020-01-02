65c5ac15d698f3d4c38e8f9624eb690ad933f973
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2020-01-02 22:29:23 +0100

    Add loading of tile elements

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,19 @@
 import 'phaser';
+import { Levels } from './levels';
+import { LevelLayout, LevelElements } from './level';
+import { ElementCode, ElementCodeSolid, ElementTile } from './level-definition';
 
 export default class Level extends Phaser.Scene
 {   
     ball;
     gameStarted = false;
 
+    levelLayout : LevelLayout;
+
     constructor ()
     {
         super('Traz - work from there');
+        this.levelLayout = new LevelLayout(Levels.levelDimension);
     }
 
     preload ()
@@ -17,13 +23,39 @@ export default class Level extends Phaser.Scene
 
     create ()
     {
-        this.ball = this.physics.add.image(400, 500, 'assets', 'ball').setCollideWorldBounds(true).setBounce(1);  
-   
+        this.createTiles(Levels.LevelDemoAssets);
+        
+        this.ball = this.physics.add.image(50, 50, 'assets', 'ball').setCollideWorldBounds(true).setBounce(1);  
 
         this.input.keyboard.on('keydown', this.handleGameToggle, this);
-
         this.input.on('pointerup', this.handleGameToggle, this);
     }
+
+    createTiles(level) {
+        for(let h = 0; h < this.levelLayout.rows; h++) {
+          for(let w = 0; w < this.levelLayout.colums; w++) {
+            var brick = level[w + (h * this.levelLayout.colums)];
+            if (brick != 0)
+            {
+              let element = new LevelElements(brick.toString());
+
+              if (element.type === ElementCode.Ball) {
+                //LevelUx.createBall(w+1,h+1);
+              } else if (element.type === ElementCode.Bonus) {
+                //Crafty.e('Bonus').setType(strCode).at(w+1,h+1);
+              } else if (element.type === ElementCode.Pad) {
+                //Crafty.e('Pad, HorizontalCollision').setSize(parseInt(strCode[3],10)).at(w+1,h+1);
+              }
+              else
+              {
+                console.log(w,h, this.levelLayout.coordX(w), this.levelLayout.coordY(h));
+                this.physics.add.image(this.levelLayout.coordX(w), this.levelLayout.coordY(h), 'assets', element.spriteCode()).setCollideWorldBounds(true)
+              }
+              
+            }
+          }
+        }
+    }   
 
     handleGameToggle() {
         if(!this.gameStarted){
@@ -36,8 +68,8 @@ export default class Level extends Phaser.Scene
 const config = {
     type: Phaser.WEBGL,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    width: Levels.levelDimension.width * 10,
+    height:  (Levels.levelDimension.height * 10)-4,
     scene: Level,
     physics: {
         default: 'arcade'
---
+++ src/level-definition.ts
@@ -0,0 +1,20 @@
+export let ElementCode = {
+    Solid: '0',
+    Passthrough: '1',
+    Pad: '2',
+    Ball: '3',
+    Bonus: '4',
+    Tile: '5'
+  }
+  
+export let  ElementCodeSolid = {
+    Brick: '0',
+    Pid: '1',
+    Spacebox: '2'
+  }
+  
+export let  ElementTile = {
+    Horizontal: '0',
+    Vertical: '1'
+  }
+  
