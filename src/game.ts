2074fe360881fd87f4e6708f996085b45203e98b
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2020-01-02 20:55:31 +0100

    Setup: Remove demo code

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,30 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
+export default class Level extends Phaser.Scene
+{   
     constructor ()
     {
-        super('demo');
+        super('Traz - work from there');
     }
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
     }
 }
 
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     backgroundColor: '#125555',
     width: 800,
     height: 600,
-    scene: Demo
+    scene: Level,
+    physics: {
+        default: 'arcade'
+    }
 };
 
 const game = new Phaser.Game(config);
